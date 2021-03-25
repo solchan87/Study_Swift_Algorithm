0b610e2f271645c314bab4bfdbb8ca91365b1442
solchan87 <perudot@gmail.com>
2021-03-25 22:35:09 +0900

    Temp

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -29,9 +29,16 @@ func solution2(_ clothes:[[String]]) -> Int {
 
 //solution([["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]])
 //solution2([["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]])
-func solution3(_ genres:[String], _ plays:[Int]) -> [Int] {
-  var genreList = Dictionary(zip(genres, plays.enumerated()), uniquingKeysWith: { return $0.element > $1.element ? $0: $1 }).sorted(by: { $0.value.element > $1.value.element }).compactMap { $0.value.offset }
-  return genreList
+
+//func solution3(_ genres:[String], _ plays:[Int]) -> [Int] {
+//  let genreList = Dictionary(zip(genres, plays.enumerated()), uniquingKeysWith: { return $0.element > $1.element ? $0: $1 }).sorted(by: { $0.value.element > $1.value.element }).compactMap { $0.value.offset }
+//  return genreList
+//}
+
+func solution4(_ genres:[String], _ plays:[Int]) -> [Int] {
+  Dictionary(zip(genres, plays.enumerated()), uniquingKeysWith: { return $0.element > $1.element ? $0: $1 })
+    
+  return [12]
 }
 
 solution3(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500])
