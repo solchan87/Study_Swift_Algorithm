01e3b26670a8fd420b916b323299e5c707625fe0
solchan87 <perudot@gmail.com>
2021-05-13 22:33:31 +0900

    Sorting

---
+++ SwiftAlgorithms/Swift Data Structure an Algorithms/C4_Sorting_Algorithms.playground/Pages/Quick Sort Hoare with Median of Three.xcplaygroundpage/Contents.swift
@@ -0,0 +1,68 @@
+import Foundation
+
+var greeting = "Hello, playground"
+
+public func quickSort<T: Comparable>(_ list: inout [T], lo: Int, hi: Int) {
+    
+    if lo < hi {
+      let median = getMedianOfThree(&list, lo: lo, hi: hi)
+      let pivot = partition(&list, lo: lo, hi: hi, median: median)
+      quickSort(&list, lo: lo, hi: pivot)
+      quickSort(&list, lo: pivot + 1, hi: hi)
+      
+  }
+}
+
+private func getMedianOfThree<T: Comparable>(_ list: inout [T], lo: Int, hi: Int) -> T {
+  
+  let center = lo + (hi - lo) / 2
+  
+  if list[lo] > list[center] {
+      swap(&list[lo], &list[center])
+  }
+  if list[lo] > list[hi] {
+      swap(&list[lo], &list[hi])
+  }
+  if list[center] > list[hi] {
+      swap(&list[center], &list[hi])
+  }
+  swap(&list[center], &list[hi])
+  
+  return list[hi]
+}
+
+private func partition<T: Comparable>(_ list: inout [T], lo: Int, hi: Int, median: T) -> Int {
+    
+    var i = lo - 1
+    var j = hi + 1
+    //: Create infinate loop
+    while true {
+
+      i += 1
+      while list[i] < median { i += 1 }
+
+      j -= 1
+      while list[j] > median { j -= 1 }
+
+      if i >= j {
+          return j
+      }
+      swap(&list[i], &list[j])
+  }
+
+}
+
+var arr = makeArray(n: 11)
+
+arr
+
+
+var startSort = Date.timeIntervalSinceReferenceDate
+
+quickSort(&arr, lo: 0, hi: arr.count - 1)
+
+var totalSortTime = Date.timeIntervalSinceReferenceDate - startSort
+
+arr
+
+totalSortTime
