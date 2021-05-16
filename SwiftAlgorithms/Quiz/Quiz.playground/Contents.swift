ae61378cf338d0e4a1e01c277a8ed259c3b1f505
solchan87 <perudot@gmail.com>
2021-05-16 17:43:44 +0900

    quiz

--- SwiftAlgorithms/Quiz/Quiz.playground/Contents.swift
+++ SwiftAlgorithms/Quiz/Quiz.playground/Contents.swift
@@ -3,8 +3,7 @@ import UIKit
 
 import UIKit
 
- let jj = 1
-let color1 = UIColor(white: jj / 255, alpha: 1.0)
+let color1 = UIColor(white: 1 / 255, alpha: 1.0)
 let color2 = UIColor(white: 1.0 / 255.0, alpha: 1.0)
 
 let result = (color1 == color2)
