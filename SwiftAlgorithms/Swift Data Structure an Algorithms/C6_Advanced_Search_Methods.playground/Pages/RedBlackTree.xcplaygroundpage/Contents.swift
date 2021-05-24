e4a17a4cf64468c8b617ee0432fcffa9e6852f0b
solchan87 <perudot@gmail.com>
2021-05-24 23:20:21 +0900

    ??

--- SwiftAlgorithms/Swift Data Structure an Algorithms/C6_Advanced_Search_Methods.playground/Pages/RedBlackTree.xcplaygroundpage/Contents.swift
+++ SwiftAlgorithms/Swift Data Structure an Algorithms/C6_Advanced_Search_Methods.playground/Pages/RedBlackTree.xcplaygroundpage/Contents.swift
@@ -15,3 +15,20 @@ rootNode.insertNodeFromRoot(value: 2)
 
 //: Print the tree
 RedBlackTreeNode.printTree(nodes: [rootNode])
+
+let redBlackTree = RedBlackTree<Double>()
+
+
+redBlackTree.insert(key: 10)
+redBlackTree.insert(key: 12)
+redBlackTree.insert(key: 5)
+redBlackTree.insert(key: 3)
+redBlackTree.insert(key: 8)
+redBlackTree.insert(key: 30)
+redBlackTree.insert(key: 11)
+redBlackTree.insert(key: 32)
+redBlackTree.insert(key: 4)
+redBlackTree.insert(key: 2)
+
+
+redBlackTree.verify()
