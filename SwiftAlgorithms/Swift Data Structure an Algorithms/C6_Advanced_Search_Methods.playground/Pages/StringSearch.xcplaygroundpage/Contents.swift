71b5494f9e57abf7850445f25ffbbf6f1ca31d1d
solchan87 <perudot@gmail.com>
2021-05-25 22:36:31 +0900

    Study

--- SwiftAlgorithms/Swift Data Structure an Algorithms/C6_Advanced_Search_Methods.playground/Pages/StringSearch.xcplaygroundpage/Contents.swift
+++ SwiftAlgorithms/Swift Data Structure an Algorithms/C6_Advanced_Search_Methods.playground/Pages/StringSearch.xcplaygroundpage/Contents.swift
@@ -4,6 +4,9 @@ StringSearch.bruteForce(search: ["3","4"], in: ["1","2","3","4","5","6"])
 
 let text = "2359023141526739921"
 let pattern = "31415"
+//let text = "asdjasidjasodoasdolasd"
+//let pattern = "idjas"
 let modulo = 13
 let base = 10
 StringSearch.rabinKarpNumbers(search: pattern, in: text, modulo: modulo, base: base)
+
